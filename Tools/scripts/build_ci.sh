safana-me/Ardu-pilot

build_ci trusted flight

Tools/scripts/build_ci.sh
@@ -319,6 +319,22 @@ for t in $CI_BUILD_TARGET; do
         continue
     fi
 
+    if [ "$t" == "CubeOrange-TFL" ]; then
+        echo "Building CubeOrange-TrustedFlight"
+        sudo apt-get update
+        sudo apt-get install -y python3-dev
+        python3 -m pip install pymonocypher==3.1.3.2
+        # generate root cert and use during build
+        Tools/scripts/AP_TrustedFlight/generate_key_and_token.py ${BUILDROOT}
+
+        # key path and token issuer defined in Tools/scripts/AP_TrustedFlight/utils/constants.py
+        $waf configure --board CubeOrange-TFL --trusted-flight-issuer=leaf.cname --trusted-flight-key=${BUILDROOT}/key.pub
+        $waf clean
+        $waf copter
+        $waf plane
+        continue
+    fi
+
     if [ "$t" == "CubeOrange-PPP" ]; then
         echo "Building CubeOrange-PPP"
         $waf configure --board CubeOrange --enable-PPP

Hurannn/Ardupilot

Tools/scripts/build_ci.sh
@@ -179,6 +179,13 @@ for t in $CI_BUILD_TARGET; do
         run_autotest "Blimp" "build.Blimp" "test.Blimp"
         continue
     fi
+    if [ "$t" == "sitltest-trusted-flight" ]; then
+        sudo apt-get update
+        sudo apt-get install -y python3-dev
+        python3 -m pip install pymonocypher==3.1.3.2
+        run_autotest "Copter" "build.TrustedFlight" "test.TrustedFlight"
+        continue
+    fi
 
     if [ "$t" == "unit-tests" ]; then
         run_autotest "Unit Tests" "build.unit_tests" "run.unit_tests"
@@ -327,6 +334,22 @@ for t in $CI_BUILD_TARGET; do
     if [ "$t" == "CubeOrange-ODID" ]; then
         echo "Building CubeOrange-ODID"
         $waf configure --board CubeOrange-ODID
+        $waf clean
+        $waf copter
+        $waf plane
+        continue
+    fi
+
+    if [ "$t" == "CubeOrange-TFL" ]; then
+        echo "Building CubeOrange-TrustedFlight"
+        sudo apt-get update
+        sudo apt-get install -y python3-dev
+        python3 -m pip install pymonocypher==3.1.3.2
+        # generate root cert and use during build
+        libraries/AP_TrustedFlight/tools/generate_key_and_token.py ${BUILDROOT}
+
+        # key path and token issuer defined in Tools/scripts/AP_TrustedFlight/utils/constants.py
+        $waf configure --board CubeOrange-TFL --trusted-flight-issuer=test.cname --trusted-flight-key=${BUILDROOT}/key.pub
         $waf clean
         $waf copter
         $waf plane